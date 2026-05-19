cb507b291aef45af74a940a71ca9c8541d269da0
tahiamubin <tahia.mubin1@gmail.com>
2026-05-19 16:09:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("pawfeet");
+    const allpetCollection = db.collection("allpets");
+
+    app.get("/all-pet", async (req, res) => {
+      const result = await allpetCollection.find().toArray();
+      res.json(result);
+    });
+
+    app.post("/all-pet", async (req, res) => {
+      const allpetData = req.body;
+      console.log(allpetData);
+      const result = await allpetCollection.insertOne(allpetData);
+      res.json(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
