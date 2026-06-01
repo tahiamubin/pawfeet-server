54a56a7d980a1b2890e772f807412178d0a82b81
tahiamubin <tahia.mubin1@gmail.com>
2026-06-01 21:32:45 +0600

    error solved

--- index.js
+++ index.js
@@ -21,28 +21,47 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+// const JWKS = createRemoteJWKSet(
+//   new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+// );
+// const verifyToken = async (req, res, next) => {
+//   const authHeader = req?.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+
+//   const token = authHeader.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+
+//   try {
+//     const { payload } = await jwtVerify(token, JWKS);
+//     next(); // make payload available to downstream handlers
+//   } catch (error) {
+//     return res.status(403).json({ message: "Forbidden" });
+//   }
+
+// };
 const JWKS = createRemoteJWKSet(
   new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
 );
+
 const verifyToken = async (req, res, next) => {
   const authHeader = req?.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(404).json({ message: "unauthorized" });
   }
-
   const token = authHeader.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(404).json({ message: "unauthorized" });
   }
-
   try {
     const { payload } = await jwtVerify(token, JWKS);
-    next(); // make payload available to downstream handlers
+    next();
   } catch (error) {
-    return res.status(403).json({ message: "Forbidden" });
+    return res.status(404).json({ message: "unauthorized" });
   }
-
- 
 };
 
 async function run() {
